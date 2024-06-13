81b87cf4916e3d5bf579cb05367dbd8096f07a82
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-06-12 19:19:08 -0500

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-Module that exports data in the CSV format
+Module that export data in the CSV format
 """
 import csv
 import requests
 from sys import argv
 
 if __name__ == '__main__':
-    user_id = int(argv[1])
-    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
-    username = user_response.json().get('username')
-    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos')
-    user_tasks = [task for task in todos_response.json() if task.get('userId') == user_id]
+    id = int(argv[1])
+    emp_data = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
+    usr_name = emp_data.json().get('username')
+    tasks_data = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+    data_all = [boole for boole in tasks_data.json()
+                if boole.get('userId') == id]
 
-    with open(f"{user_id}.csv", 'a', newline='', encoding="utf-8") as csvfile:
-        csv_writer = csv.writer(csvfile, dialect="unix")
-        for task in user_tasks:
-            csv_writer.writerow([task['userId'], username,
-                                 task['completed'], task['title']])
+    with open(f"{id}.csv", 'a', newline='', encoding="utf-8") as csvfile:
+        write = csv.writer(csvfile, dialect="unix")
+        for data in data_all:
+            write.writerow([data['userId'], usr_name,
+                            data['completed'], data['title']])
