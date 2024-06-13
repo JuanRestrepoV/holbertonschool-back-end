86f1782cc197ff706790886fa00b1a23fd013837
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-06-12 22:40:32 -0500

    task 3

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -11,20 +11,17 @@ if __name__ == '__main__':
 
     unique_user_ids = list(set([task['userId'] for task in data]))
     user_tasks_dict = {}
-    
     for user_id in unique_user_ids:
         user_info = requests.get(
             f'https://jsonplaceholder.typicode.com/users/{user_id}').json()
         user_tasks = requests.get(
-            f'https://jsonplaceholder.typicode.com/users/{user_id}/todos').json()
-        
+            f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+            ).json()
         formatted_tasks = [{
             "username": user_info['username'],
             "task": task['title'],
             "completed": task['completed']
         } for task in user_tasks]
-        
         user_tasks_dict[user_id] = formatted_tasks
-
     with open('todo_all_employees.json', 'w', encoding='utf-8') as file:
         json.dump(user_tasks_dict, file)
