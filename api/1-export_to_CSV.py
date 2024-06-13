9a9800bd0238843e06d78eec31a936a92acec944
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-06-12 21:25:55 -0500

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,10 +8,12 @@ from sys import argv
 
 if __name__ == '__main__':
     user_id = int(argv[1])
-    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
+    user_response = requests.get(f'https://jsonplaceholder.typicode.com/
+                                 users/{user_id}')
     username = user_response.json().get('username')
 
-    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/
+                                  todos')
     user_tasks = [task for task in todos_response.json() if task.get('userId') == user_id]
 
     with open(f"{user_id}.csv", 'a', newline='', encoding="utf-8") as csvfile:
